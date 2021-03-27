94432d75866dc5d77675c2f369f0bcfa2993e0b5
filiprrs <filiprrs@gmail.com>
2021-03-27 23:44:00 +0100

    Fix a mismerge
    Change-Id: Ic08a773ffa59a1fda459c2c8c4394bfdda0767a3

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -645,7 +645,7 @@ void __ext4_abort(struct super_block *sb, const char *function,
 		sb->s_flags |= MS_RDONLY;
 		if (EXT4_SB(sb)->s_journal)
 			jbd2_journal_abort(EXT4_SB(sb)->s_journal, -EIO);
-		save_error_info(sb, function, line);F
+		save_error_info(sb, function, line);
 	}
 	if (test_opt(sb, ERRORS_PANIC) && !ignore_fs_panic) {
 		if (EXT4_SB(sb)->s_journal &&
