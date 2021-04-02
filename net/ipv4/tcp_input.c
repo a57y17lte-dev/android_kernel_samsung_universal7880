016eb20031632e6dc372df9d54d31077b5c4d112
filiprrs <filiprrs@gmail.com>
2021-04-02 18:07:54 +0200

    Fix a mismerge
    Change-Id: I83c8118ce71941dd2750ad24a4d86c9a4c4297d7

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -390,12 +390,18 @@ static int __tcp_grow_window(const struct sock *sk, const struct sk_buff *skb)
 static void tcp_grow_window(struct sock *sk, const struct sk_buff *skb)
 {
 	struct tcp_sock *tp = tcp_sk(sk);
-	int room;
-
-	room = min_t(int, tp->window_clamp, tcp_space(sk)) - tp->rcv_ssthresh;	
 #ifdef CONFIG_MPTCP
 	struct sock *meta_sk = mptcp(tp) ? mptcp_meta_sk(sk) : sk;
 	struct tcp_sock *meta_tp = tcp_sk(meta_sk);
+#endif
+	int room;
+#ifdef CONFIG_MPTCP
+	if (is_meta_sk(sk))
+		return;
+
+	room = min_t(int, meta_tp->window_clamp, tcp_space(meta_sk)) - meta_tp->rcv_ssthresh;
+#else
+	room = min_t(int, tp->window_clamp, tcp_space(sk)) - tp->rcv_ssthresh;
 #endif
 
 	/* Check #1 */
